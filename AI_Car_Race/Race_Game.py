26f13c31094cf0173a0459916f51c056ec11651f
Goldencode20 <avatarcreed1@gmail.com>
2023-04-25 15:49:09 -0400

    Race Update

--- AI_Car_Race/Race_Game.py
+++ AI_Car_Race/Race_Game.py
@@ -1,1 +1,30 @@
-#FIND GUI THAT CAN TAKE IN KEYSTROKE
+"""
+Author: Creed Jones
+
+Summary:
+
+Next Steps: 
+    Get image files working 
+    Place image on screen
+"""
+
+import tkinter
+from tkinter import * 
+
+# Set up screen
+screen = tkinter.Tk()
+screen.title('Race Game')
+screen.geometry("500x500")
+
+# Do this when key is pressed
+def key_press(e):
+   #label.config(text="Hello World!")
+   label.config(text = e.keysym) # Return the key pressed
+
+# Create a label widget to add some text
+label= Label(screen, text = "", font = ('Helvetica 17 bold'))
+label.pack(pady = 50)
+
+# Bind the key press and run when key is pressed
+screen.bind('<KeyPress>', key_press)
+screen.mainloop()
