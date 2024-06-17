5ba8504a16f8215e29a4256a35c9e16ed1212748
Goldencode20 <avatarcreed1@gmail.com>
2024-06-17 15:55:02 -0400

    Start of Visual Basic

---
+++ LinkedIn Learning/Visual Basic Essential Training/SimpleConsole/SimpleConsole/Module1.vb
@@ -0,0 +1,9 @@
+﻿Module Module1
+
+  Sub Main()
+    Console.Write("Today is ... ")
+    Console.ForegroundColor = ConsoleColor.Yellow
+    Console.WriteLine(DateTime.Now.DayOfWeek.ToString)
+  End Sub
+
+End Module
